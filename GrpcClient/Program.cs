f87905517bad238c02000c69265dacb0b76a544e
DAVID\Mariya Pirinova <mpirinova@david.bg>
2023-11-29 16:22:47 +0200

    Add methods as communication modes representations

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,17 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Google.Protobuf.Collections;
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcServer;
 
-class Program
+public static class Program
 {
     static async Task Main(string[] args)
     {
         using var channel = GrpcChannel.ForAddress("https://localhost:7292");
         var client = new Greeter.GreeterClient(channel);
 
-        var reply = await client.SayHelloAsync(new HelloRequest { Name = "Client" });
+        // Unary
+        //var customer = await client.GetCustomerAsync(new CustomerId { Id = 1 });
+        //customer.DisplayCustomer();
 
-        Console.WriteLine($"Greeting: {reply.Message}");
-        Console.ReadKey();
+        // Server streaming
+        //using (var call = client.GetAllCustomers(new AllCustomersRequest()))
+        //{
+        //    while (await call.ResponseStream.MoveNext())
+        //    {
+        //        var currentCustomer = call.ResponseStream.Current;
+        //        currentCustomer.DisplayCustomer();
+        //    }
+        //}
+
+        // Client streaming
+        //var products = new List<Product>()
+        //{
+        //    new Product
+        //    {
+        //        Name = "Banana",
+        //        Price = 1.25f,
+        //    },
+        //    new Product
+        //    {
+        //        Name = "Apple",
+        //        Price = 0.85f,
+        //    },
+        //    new Product
+        //    {
+        //        Name = "Orange",
+        //        Price = 1.00f,
+        //    }
+        //};
+
+        //using var call = client.GetProductsTotalPrice();
+
+        //foreach (var product in products)
+        //{
+        //    Console.WriteLine($"Entering product: {product.Name}");
+        //    await call.RequestStream.WriteAsync(product);
+        //    await Task.Delay(1000);
+        //}
+
+        //await call.RequestStream.CompleteAsync();
+        //var response = await call;
+        //Console.WriteLine($"Total price is {response.TotalPrice_}");
+
+        // Bidirectional streaming
+
+        using (var call = client.GetPriceByProductName())
+        {
+
+        }
+    }
+
+    private static void DisplayCustomer(this Customer customer)
+    {
+        Console.WriteLine($"Name: {customer.Name}");
+        Console.WriteLine($"Age: {customer.Age}");
+        Console.WriteLine();
+        foreach (var product in customer.Products)
+        {
+            Console.WriteLine($"    Name: {product.Name}");
+            Console.WriteLine($"    Price: {product.Price}");
+        }
     }
 }
--- GrpcServer/Services/GreeterService.cs
+++ GrpcServer/Services/GreeterService.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.Collections;
 using Grpc.Core;
 
 namespace GrpcServer.Services
@@ -10,12 +11,111 @@ namespace GrpcServer.Services
             _logger = logger;
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public override Task<Customer> GetCustomer(CustomerId id, ServerCallContext context)
         {
-            return Task.FromResult(new HelloReply
+            var products = new List<Product>()
             {
-                Message = "Hello " + request.Name
-            });
+                new Product
+                {
+                    Name = "Banana",
+                    Price = 1.25f,
+                },
+                new Product
+                {
+                    Name = "Apple",
+                    Price = 0.85f,
+                },
+                new Product
+                {
+                    Name = "Orange",
+                    Price = 1.00f,
+                }
+            };
+
+            var reply = new Customer
+            {
+                Name = "Jane Doe",
+                Age = 26,
+            };
+            reply.Products.AddRange(products);
+
+            return Task.FromResult(reply);
+        }
+
+        public override async Task GetAllCustomers(AllCustomersRequest request, IServerStreamWriter<Customer> responseStream, ServerCallContext context)
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    Name = "Jane Doe",
+                    Age = 26,
+                },
+                new Customer
+                {
+                    Name = "Peter Parker",
+                    Age = 24,
+                },
+                new Customer
+                {
+                    Name = "Tony Stark",
+                    Age = 44,
+                },
+            };
+
+            foreach (var item in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+        public override async Task<TotalPrice> GetProductsTotalPrice(IAsyncStreamReader<Product> requestStream, ServerCallContext context)
+        {
+            float totalPrice = 0.0f;
+
+            while (await requestStream.MoveNext())
+            {
+                var product = requestStream.Current;
+                Console.WriteLine($"Received product: {product.Name}");
+                totalPrice += product.Price;
+            }
+
+            return await Task.FromResult(new TotalPrice { TotalPrice_ = totalPrice, });
+        }
+
+        public override async Task GetPriceByProductName(IAsyncStreamReader<ProductName> requestStream, IServerStreamWriter<ProductPrice> responseStream, ServerCallContext context)
+        {
+            var products = new List<Product>()
+            {
+                new Product
+                {
+                    Name = "Banana",
+                    Price = 1.25f,
+                },
+                new Product
+                {
+                    Name = "Apple",
+                    Price = 0.85f,
+                },
+                new Product
+                {
+                    Name = "Orange",
+                    Price = 1.00f,
+                }
+            };
+
+            while (await requestStream.MoveNext())
+            {
+                var current = requestStream.Current;
+                var productPrice = products.Where(x => x.Name == current.Name).Select(x => x.Price).FirstOrDefault();
+                var reply = new ProductPrice
+                {
+                    Price = productPrice
+                };
+
+                await responseStream.WriteAsync(reply);
+            }
         }
     }
 }
