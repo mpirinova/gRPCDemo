fb7cfd12f89762f7f28006534064e942b9eff32c
DAVID\Mariya Pirinova <mpirinova@david.bg>
2023-11-29 10:36:30 +0200

    Add client project

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcServer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:7292");
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "Client" });
+
+        Console.WriteLine($"Greeting: {reply.Message}");
+        Console.ReadKey();
+    }
+}
